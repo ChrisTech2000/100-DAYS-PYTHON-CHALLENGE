0641044c311cad2c796a101600e1573b857447d4
Christian <projectoffice@manamuz.com>
2026-04-09 07:20:00 +0100

    first commit

---
+++ daily_challenge.py
@@ -0,0 +1,10 @@
+#userName = input("Enter your name: ")
+
+#length = len(userName)
+#print(length)
+
+print("Welcome to Band Name Generator")
+grewUpCity = input("What is the city you grew up from?\n")
+petName = input("What is your pet name?\n")
+combinedName = grewUpCity + " " + petName
+print("Your Band Name is "+combinedName)
